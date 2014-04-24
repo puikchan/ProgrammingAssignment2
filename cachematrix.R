75602ab237c0a9b586ec2e9ece097ce00ec56e40
Pui Chan <pchan@cablevision.com>
2014-04-23 23:14:10 -0400

    committing file cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function makeCacheMatrix
+## It takes an argument x ot type numeric matrix
+## It returns a list of 4 functions
+## 	 list (set = set, get - get, getInv = set Inv, getInv = getInv)
+## Example usage:
+## a <- makeCacheMatrix(matrix(1:4,2))
+## a$get()
+## a$getInv()
+## a$set(matrix(5:8,2))
+## a$get()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+         m <- NULL
+         set <- function(y) {
+                 x <<- y
+                 m <<- NULL
+         }
+         get <- function() x
+         setInv <- function(solve) m <<- solve
+         getInv <- function() m
+         list(set = set, get = get,
+              setInv = setInv,
+              getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve -- returns a matrix that is the inverse of argument x
+## Example usage:
+## a <- makeCacheMatrix(matrix(1:4,2))
+## cacheSolve(a)
+## cacheSolve(a)
+## a$getInv()
+## b = a$getInv()
+## a$get() %*% b
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   m <- x$getInv()             # query the x Inverse Matrix's cache         
+   if(!is.null(m)) {           # if there is a cache
+     message("getting cached data") 
+     return(m)                 # just return the cache, no computation needed
+   }
+   data <- x$get()             # if there's no cache
+   m <- solve(data, ...)       # we actually compute them here
+   x$setInv(m)                 # save the result back to x's cache
+   m                           # return the result
 }
